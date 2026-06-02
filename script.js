39991ed666c880d7555bb7f89b21541658551241
RAM MANOHAR REDDY <malirammanoharreddy@gmail.com>
2026-06-02 12:23:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. 
+const height = document.getelementbyId("height")
+const width = document.getelementbyId("width")
+height.innerText =window.innerheight;
+width.innerText =window.innerwidth;
+
+window.addEventListener("resize", function(){
+            height.innerText = window.innerHeight;
+            width.innerText = window.innerWidth;
+        })
